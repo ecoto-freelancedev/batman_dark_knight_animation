8d2071c2f8e7c70a94cfb9ddb17b0a1ba832e010
ecoto17 <ecoto30@gmail.com>
2020-10-13 20:04:32 -0600

    Asset added and main file modified

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,34 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _bigger = false;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
+      title: 'Flutter Animation',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Material App Bar'),
+          backgroundColor: Colors.black,
+          title: Text('Batman Dark Knight Animation'),
         ),
         body: Center(
-          child: Container(
-            child: Text('Hello World'),
+          child: GestureDetector(
+            child: Container(
+              width: _bigger ? 100 : 500,
+              child: Image.asset('images/batman_logo.jpg'),
+            ),
+            onTap: () {
+              setState(() {
+                _bigger = !_bigger;
+              });
+            },
           ),
         ),
       ),
